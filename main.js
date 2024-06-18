95de1bd8f1a7e51fe5ab39d88b800efd5e8bfd91
brlegi <feduardosg@gmail.com>
2024-06-18 12:19:54 -0300

    Add a keyboard input function

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+var result = 0;
+var acumulator = 0;
+let displayn = document.getElementById("displaynum");
+
+function calc(initn, secondn, operator) {
+    
+    switch(operator) {
+        case sum:
+            result = initn + secondn;
+            break;
+        case sub:
+            result = initn - secondn;
+            break;
+        case mult:
+            result = initn * secondn;
+            break;
+        case division:
+            result = initn / secondn;
+            break;
+    }
+
+}
+
+function getInput(num) {
+    let displayn = document.getElementById("displaynum");
+    displayn.innerHTML = num;
+    console.log(num);
+}
+
 
